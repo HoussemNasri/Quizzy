69208a1155887b53ee20ee45bd27968f4d635f83
Houssemnasri <housi.housi2015@gmail.com>
2020-03-26 02:23:11 +0100

    designing the quiz activity

--- app/src/main/java/com/example/quizzy/ui/MainActivity.java
+++ app/src/main/java/com/example/quizzy/ui/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.quizzy.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.quizzy.R;
@@ -21,18 +23,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void userChoosedCategory(View view) {
-        String viewTag = view.getTag().toString();
+        Object tag = view.getTag();
         int viewId = view.getId();
         int categoryId = -1;
-        if (!viewTag.isEmpty()) {
+        if (tag != null) {
+            String viewTag = tag.toString();
             categoryId = CategoryHelper.getCategoryId(viewTag);
         } else {
+            Log.d(TAG, "userChoosedCategory: Empty Tag");
             categoryId = CategoryHelper.getRandomCategoryId();
         }
 
         startQuizActivity(categoryId);
 
     }
+
 
     public void startQuizActivity (int categoryId) {
         Intent quizActivity = new Intent(this, QuizActivity.class);
--- app/src/main/java/com/example/quizzy/ui/QuizActivity.java
+++ app/src/main/java/com/example/quizzy/ui/QuizActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.quizzy.R;
 import com.example.quizzy.helpers.CategoryHelper;
@@ -12,18 +15,35 @@ public class QuizActivity extends AppCompatActivity {
 
     private String currentCategoryName = "";
     private int currentCategoryId = -1;
+
+    private TextView toolbarTitle;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        receivceCategoryData();
+        receiveingCategoryData();
 
+        toolbarTitle = findViewById(R.id.quiz_toolbar_title);
+        toolbarTitle.setText(currentCategoryName);
+        settingUpToolbar();
     }
 
-    public void receivceCategoryData () {
+    public void receiveingCategoryData() {
         Intent intent = getIntent();
         currentCategoryId = intent.getIntExtra(MainActivity.CATEGORY_KEY, -1);
         currentCategoryName = CategoryHelper.getCategoryName(currentCategoryId);
+    }
+
+
+    public void settingUpToolbar() {
+        toolbar = findViewById(R.id.quiz_toolbar);
+        setActionBar(toolbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(e -> onBackPressed());
+
     }
 
 
